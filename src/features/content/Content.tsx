57102c491500d117ca15a1d94643e0b4132261b1
Perez Githumbi <codakniggz145@gmail.com>
2024-07-01 17:52:40 +0300

    cleanup eslint errors

--- src/features/content/Content.tsx
+++ src/features/content/Content.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./About";
 import Homepage from "./Homepage";
 import SearchPage from "./SearchPage";
+import Portfolio from "./Portfolio";
 
 const Content = () => {
   return (
@@ -12,6 +13,7 @@ const Content = () => {
         <Route path="/about" element={<About />}></Route>
         <Route path="/homepage" element={<Homepage />}></Route>
         <Route path="/search" element={<SearchPage />}></Route>
+        <Route path="/portfolio" element={<Portfolio />}></Route>
       </Routes>
     </div>
   );
